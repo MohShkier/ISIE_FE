c220864fdb5b26449b6b4a61bb3e7c002f51cf71
Mohammed Sh <65793511+MohShkier@users.noreply.github.com>
2025-02-23 15:43:40 +0300

    Update clientItem.jsx

--- src/components/OurClients/clientItem.jsx
+++ src/components/OurClients/clientItem.jsx
@@ -1,13 +1,11 @@
 function ClientItem({ imgSrc, title }) {
     return (
-       <>
         <div className="flex flex-col items-center bg-white rounded-[20px] xl:size-[15rem] shadow-[2px_2px_40px_2px_rgba(0,0,0,0.25)]">
-            <img src={imgSrc} className="px-3 py-4" />
-            <p className="text-center text-[#1D2736]  xl:text-[2rem] text-lg flex-grow flex items-center justify-center w-full ">
+            <img src={imgSrc} className="px-2 py-2 xl:px-3 xlpy-4" />
+            <p className="text-center text-[#1D2736]  xl:text-[2rem] text-lg flex-grow flex items-center justify-center w-full">
                 {title}
             </p>
         </div>
-       </>
     );
 }
 
