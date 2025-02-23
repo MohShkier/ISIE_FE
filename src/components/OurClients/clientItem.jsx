97d0d26be6a2e1961583cb7095440209905ddeb6
Mohammed Sh <65793511+MohShkier@users.noreply.github.com>
2025-02-23 15:38:41 +0300

    Update clientItem.jsx

--- src/components/OurClients/clientItem.jsx
+++ src/components/OurClients/clientItem.jsx
@@ -3,7 +3,7 @@ function ClientItem({ imgSrc, title }) {
        <>
         <div className="flex flex-col items-center bg-white rounded-[20px] xl:size-[15rem] shadow-[2px_2px_40px_2px_rgba(0,0,0,0.25)]">
             <img src={imgSrc} className="px-3 py-4" />
-            <p className="text-center text-[#1D2736]  xl:text-[2rem] text-xl flex-grow flex items-center justify-center w-full ">
+            <p className="text-center text-[#1D2736]  xl:text-[2rem] text-lg flex-grow flex items-center justify-center w-full ">
                 {title}
             </p>
         </div>
