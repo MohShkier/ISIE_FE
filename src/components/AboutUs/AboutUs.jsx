e2a584e0e4c0e064baa3617e4e0941ae641a6f91
Moh <contact@mohsh.me>
2025-02-24 22:54:14 +0300

    m

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import OurClient from "../OurClients/ourClient";
 
 function AboutUs() {
     return (
@@ -164,6 +165,7 @@ function AboutUs() {
     </div>
 </div>
 
+<OurClient/>
         </>
     );
 }
