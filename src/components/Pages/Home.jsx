0a41d27fddde8c5ef65663c7f01b7cad2443fb40
Mohammed Sh <65793511+MohShkier@users.noreply.github.com>
2025-02-25 18:13:28 +0300

    Update Home.jsx

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,31 +1,10 @@
 import HeaderSwiper from "../Header/Swiper";
 import OurClient from "../OurClients/ourClient";
 import ProductSection from "../OurProductSection/OurProductSection";
-import { useState, useEffect } from "react";
 import axios from "axios"
 function Home() {
 
 
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:8000/api/home", {
-          withCredentials: true, // Allows cookies/session handling if needed
-        });
-        setData(response.data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
   return (
     <>
      <HeaderSwiper/>
