7898d5a74ec8b679b63385999671037bff68bb4d
Moh <contact@mohsh.me>
2025-02-27 13:26:28 +0300

    Filled Website title and meta data

--- src/components/SignleProduct/ProductsImgSwiper.jsx
+++ src/components/SignleProduct/ProductsImgSwiper.jsx
@@ -81,11 +81,11 @@ function ProductImgSwiper({ product }) {
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
         >
-          <h2 className="text-3xl font-bold text-gray-800 mb-6">
+          <h2 className="text-2xl sm:text-3xl lg:text-3xl font-bold text-gray-800 mb-6">
             {product.name}
           </h2>
           <p className="text-gray-700 text-lg leading-relaxed ">
-            {product.description}
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
 
           <div className="flex space-x-4 lg:justify-end justify-start pt-12 xl:pt-20">
